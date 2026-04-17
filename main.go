e42287090fb34d02f4ddcf1f6c69a9fcbf41ab36
Animesh Gandhi <animeshg93@gmail.com>
2026-04-16 22:50:35 -0500

    Added error handling

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"example/vinyl-web/models"
 	"fmt"
 	"net/http"
@@ -11,28 +12,46 @@ import (
 
 func main() {
 	router := gin.Default()
-	gituser := fetchGitUser()
 
-	router.GET("/user", func(c *gin.Context) {
+	router.GET("/user/:username", func(c *gin.Context) {
+		name := c.Param("username")
+		gituser, err := fetchGitUser(name)
+
+		if err != nil && err.Error() == "user not found" {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
 		c.JSON(http.StatusOK, gituser)
 	})
 
 	router.Run(":8080")
 }
 
-func fetchGitUser() models.GitUser {
-	resp, err := http.Get("https://api.github.com/users/animeshg93")
-	if err != nil {
-		fmt.Println("Error fetching data:", err)
-		panic("ERRORRRRR")
-	}
-	defer resp.Body.Close()
-
+func fetchGitUser(name string) (models.GitUser, error) {
+	resp, err := http.Get("https://api.github.com/users/" + name)
 	var gitUser models.GitUser
+
+	if resp.StatusCode == http.StatusNotFound {
+		return models.GitUser{}, errors.New("user not found")
+	}
+	if err != nil || resp.StatusCode != http.StatusOK {
+		fmt.Println("Error fetching data:", err)
+		return models.GitUser{}, errors.New("failed to fetch git user")
+	}
+
 	err = json.NewDecoder(resp.Body).Decode(&gitUser)
 	if err != nil {
 		fmt.Println("Error decoding JSON:", err)
 		panic(err)
 	}
-	return gitUser
+
+	defer resp.Body.Close()
+	return gitUser, nil
+
 }
